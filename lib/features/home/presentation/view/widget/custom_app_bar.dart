a4c67efb7a5a80e9321594c07255e084fde19d98
rand ethar <randethar@gmail.com>
2025-02-09 12:59:42 +0000

    add custom app bar

---
+++ lib/features/home/presentation/view/widget/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          const Icon(
+            Iconsax.search_normal_1,
+            size: 26,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
