6b6130ad20a2faec97e7dcce264b76b8af73c53d
randethar <randethar@gmail.com>
2025-04-26 19:39:14 +0100

    create Featch Newest Books Use Case

--- lib/features/home/domain/use_cases/featch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/featch_newest_books_use_case.dart
@@ -1,17 +1,16 @@
 import 'package:bookly_app/core/errors/failuers.dart';
 import 'package:bookly_app/core/use_cases/use_cases.dart';
-
 import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FeatchFeaturedBooksUseCase implements UseCase<List<BookEntity>, NoParam> {
+class FeatchNewestBooksUseCase implements UseCase<List<BookEntity>, NoParam> {
   final HomeRepos homeRepo;
 
-  FeatchFeaturedBooksUseCase(this.homeRepo);
+  FeatchNewestBooksUseCase(this.homeRepo);
 
   @override
   Future<Either<Failures, List<BookEntity>>> call([NoParam? param]) {
-   return homeRepo.featchFeaturedBooks();
+    return homeRepo.featchNewestBooks();
   }
 }
